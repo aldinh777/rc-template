8522584d25e2cca2324e6cc193ac6656c0268479
aldinh777 <aldi.nh789@gmail.com>
2022-10-06 18:14:33 +0800

    update default libraries

--- src/states.js
+++ src/states.js
@@ -1,7 +1,8 @@
 import { state } from '@aldinh777/reactive';
+import { stateLocalStorage } from '@aldinh777/reactive-cml/dom/reactive-util';
 
-export const navbar = state('home');
+export const navbar = stateLocalStorage('nav', 'home');
+export const mainColor = stateLocalStorage('theme', 'purple');
 
 export const counter = state(0);
 export const urmom = state('isabella');
-export const mainColor = state('purple');
