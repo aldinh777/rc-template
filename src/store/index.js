9ce1b65c15157da2bfba141931b91ab666714cd2
aldinh777 <aldi.nh789@gmail.com>
2022-10-17 15:42:28 +0800

    adding todo list as example

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import { state } from '@aldinh777/reactive';
+import { state, stateObserve } from '@aldinh777/reactive';
+import { statelist } from '@aldinh777/reactive/util/collection';
 import { stateLocalStorage } from '@aldinh777/reactive-cml/dom/reactive-util';
 
 export const navbar = stateLocalStorage('nav', 'home');
@@ -6,3 +7,8 @@ export const mainColor = stateLocalStorage('theme', 'purple');
 
 export const counter = state(0);
 export const urmom = state('isabella');
+export const todo = statelist([]);
+export const todoLength = state(todo.raw.length);
+todo.onInsert(() => (todoLength.value = todo.raw.length));
+todo.onDelete(() => (todoLength.value = todo.raw.length));
+export const todoIsEmpty = stateObserve(todoLength, (length) => length <= 0);
