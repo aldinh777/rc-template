c5e79b98e7a297f9a853f358a58a01d8e9a62726
aldinh777 <aldi.nh789@gmail.com>
2022-12-29 02:45:57 +0800

    aye sir i'm not focused im watchin fuyo right now

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import { append } from '@aldinh777/reactive-cml/dom/dom-util';
-import ComponentError from '@aldinh777/reactive-cml/error/ComponentError';
 import App from './App.rc';
 
 try {
     append(document.querySelector('#app'), App());
-} catch (e) {
-    const err = ComponentError.componentCrash('App', e);
+} catch (err) {
     const pre = document.createElement('pre');
-    pre.innerText = err;
+    pre.innerText = err.stack;
     document.body.appendChild(pre);
     console.error(err);
 }
