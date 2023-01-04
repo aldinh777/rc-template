56c9944ca5cedbe31007cefc9d9535d017efbc87
aldinh777 <aldi.nh789@gmail.com>
2023-01-04 18:12:20 +0800

    update dependency

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { mount, _doc } from '@aldinh777/reactive-cml/dom/dom-util';
+import { append, mount, _doc } from '@aldinh777/reactive-cml/dom/dom-util';
 import App from './App.rc';
 
 try {
     mount(_doc.querySelector('#app'), App());
 } catch (err) {
     const pre = document.createElement('pre');
-    pre.innerText = err.stack;
-    _doc.body.appendChild(pre);
+    pre.innerText = err.stack || err;
+    append(_doc.body, [pre]);
     console.error(err);
 }
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import { join } from 'path';
-import rcml from '@aldinh777/rollup-plugin-reactive-cml';
+import rcml from './dev-lib/rollup-plugin-reactive-cml';
 
 export default defineConfig({
     plugins: [
         rcml({
-            useDefaultLibs: true,
+            autoImports: [
+                'reactive-utils',
+                'reactive-utils/collection'
+            ],
             parserOptions: {
                 relativeImports: {
                     includes: [join(__dirname, 'src')]
