765641d4f4e676771c081becf686963fd983eec1
aldinh777 <aldi.nh789@gmail.com>
2023-02-17 22:06:06 +0800

    asynchronous render

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import { _doc, _elem, append } from '@aldinh777/reactive-cml/dom/dom-util';
 import { mount } from '@aldinh777/reactive-cml/dom';
+import ComponentError from '@aldinh777/reactive-cml/error/ComponentError';
 import App from './App.rc';
 
-try {
-    mount(_doc.querySelector('#app'), App());
-} catch (err) {
-    const pre = _elem('pre');
-    pre.innerText = err.stack || err;
-    append(_doc.body, [pre]);
-    console.error(err);
-}
+!(async function () {
+    try {
+        mount(_doc.querySelector('#app'), await App());
+    } catch (err) {
+        const blacklist = ['StateIf', 'Children'];
+        if (err.componentTraces) {
+            const cause = err.componentTraces.at(-1);
+            err = new ComponentError(
+                `crash at component '${cause}'`,
+                ['App', ...err.componentTraces].filter((comp) => !blacklist.includes(comp)),
+                err.reason
+            );
+        }
+        const pre = _elem('pre');
+        pre.innerText = err.stack || err;
+        append(_doc.body, [pre]);
+        console.error(err);
+    }
+})();
